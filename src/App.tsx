76b30f8bd76cb0e16a4d47c1022a7d1e31b6f679
Rafaela Nunes <rafaelansiqueira@gmail.com>
2026-09-23 21:12:04 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CadastroMorador from "./pages/CadastroMorador";
+import PesquisaMoradores from "./pages/PesquisaMoradores";
+import PerfilMorador from "./pages/PerfilMorador";
+import Mapa from "./pages/Mapa";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cadastro" element={<CadastroMorador />} />
+          <Route path="/pesquisa" element={<PesquisaMoradores />} />
+          <Route path="/morador/:id" element={<PerfilMorador />} />
+          <Route path="/mapa" element={<Mapa />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
