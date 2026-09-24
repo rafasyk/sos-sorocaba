86fba865ff0def7411f59f2e270f4f68914ec8a7
Rafaela Nunes <rafaelansiqueira@gmail.com>
2026-09-23 21:21:19 -0300

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
+export type Situacao = "Abrigado" | "Na rua";
 
+export type Morador = {
+  id: number;
+  nome: string;
+  documento: string;
+  dataCadastro: string;
+  situacao: Situacao;
+  abrigo: string | null;
+  latitude: number;
+  longitude: number;
+};
